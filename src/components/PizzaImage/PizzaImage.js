bdca04b31d47a8da972905ff3f09fa2939246a82
codershona <falgunimst95@gmail.com>
2020-04-14 12:39:03 +0200

    Creating the Basic React Application : method 3

--- src/components/PizzaImage/PizzaImage.js
+++ src/components/PizzaImage/PizzaImage.js
@@ -1,4 +1,15 @@
+import React from 'react';
+
+import classes from './PizzaImage.css';
+import PizzaImage from '../../assets/pizza.jpg';
+
 const pizzaImage = (props) => (
-    <div></div>
+    <div className={classes.PizzaImage}>
+        <img src={PizzaImage} className={classes.PizzaImg} />
+
+    </div>
 
 );
+
+
+export default pizzaImage;
